d768b5ace689a1244c4ab7f63e4e5dac9abed546
Beginner24082005 <114759487+Beginner24082005@users.noreply.github.com>
2023-09-12 16:10:40 +0700

    Add files via upload

---
+++ Lecture1.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Input your number:"<<endl;
+    while (true){
+        cin>>a;
+        if (a>0){
+            cout<<"You input the right number. "<<endl;
+        }
+        else{
+            cout<<"You input the wrong number. Please input again: "<<endl;
+            continue;
+        }
+        cout<<"Do you want to exit? (1/0)"<<endl;
+        cin>>b;
+        if (b==1) break;
+        else if (b==0){
+            cout<<"Input your number: "<<endl;
+            continue;
+        } 
+        else{
+            while (true){
+                cout<<"Invalid choice. Input again: "<<endl;
+                cin>>b;
+                if (b==1){
+                    break;
+                }
+                else if (b==0){
+                    break;
+                }
+                else continue;
+            }
+            if (b==1) break;
+            if (b==0){
+                cout<<"Input your number: "<<endl;
+                continue;
+            } 
+        }
+        
+    }
+    return 0;
+}
+//Trying
